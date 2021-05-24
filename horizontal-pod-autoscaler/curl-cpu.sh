d6ea5aa82e35ef6b9b8f28e9122bea359858b038
Argeu Alcantara <argeu.alcantara@hp.com>
2021-05-24 11:02:14 -0300

    Add memory hpa

---
+++ horizontal-pod-autoscaler/curl-cpu.sh
@@ -0,0 +1,1 @@
+curl --location http://argeu.cluster.com/ConsumeCPU --data "millicores=450&durationSec=60"
---
+++ horizontal-pod-autoscaler/curl-mem.sh
@@ -0,0 +1,1 @@
+curl --location http://argeu.cluster.com/ConsumeMem --data "megabytes=300&durationSec=60"
